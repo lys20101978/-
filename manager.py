9748547e74809b5b6057e4c34e198cadb9e7a2ba
lys20101978 <lys20101978@hotmail.com>
2019-06-23 20:02:07 +0800

    tree

---
+++ manager.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 22 18:37:05 2019
+
+@author: liningsheng
+"""
+import app from start
+if __name=="__main__":
+     app.run(host='0.0.0.0',port=5000)
