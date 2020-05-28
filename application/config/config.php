c43fc6a56029951d58b06e150c3b5628a07e4054
liningsheng <lys20101978@hotmail.com>
2020-05-28 16:22:11 +0800

    修改config.php ip地址

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = ' http://10.43.52.22:8080/CI';
+$config['base_url'] = ' http://192.168.100.26:8080/CI';
 
 /*
 |--------------------------------------------------------------------------
